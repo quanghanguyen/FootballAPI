33ccbcff52481b470aeab97a478b09786142b4e9
Quang Ha <hakun32@gmail.com>
2022-03-10 13:47:12 +0700

    edit-c2-10-03

--- app/src/main/java/com/example/footballapi/Home.java
+++ app/src/main/java/com/example/footballapi/Home.java
@@ -43,6 +43,8 @@ public class Home extends AppCompatActivity {
                 Log.e(TAG, "onResponse: code : " + response.body().getCurrentSeason().getStartDate());
                 Log.e(TAG, "onResponse: code : " + response.body().getCurrentSeason().getEndDate());
                 Log.e(TAG, "onResponse: code : " + response.body().getCurrentSeason().getCurrentMatchday());
+                
+
                 // Làm sao để mình có thể lấy được Data mục shortname ở trong class Winner?
                 //------------
                 //------------
