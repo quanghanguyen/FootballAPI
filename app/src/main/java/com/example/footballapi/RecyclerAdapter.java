983b4dc10cc1a45b6c97e49d0e28be117ac525a3
Quang Ha <hakun32@gmail.com>
2022-03-10 14:38:01 +0700

    edit-c3-10-03-

--- app/src/main/java/com/example/footballapi/RecyclerAdapter.java
+++ app/src/main/java/com/example/footballapi/RecyclerAdapter.java
@@ -1,5 +1,6 @@
 package com.example.footballapi;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,47 +9,64 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.footballapi.PL.PLModel;
+
 import java.util.List;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    //private List
+    private List<PLModel> arrayList;
+    private Context context;
+
+    public RecyclerAdapter(List<PLModel> arrayList, Context context) {
+        this.arrayList = arrayList;
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View inflate = layoutInflater.inflate(R.layout.list_home, null);
         ViewHolder viewHolder = new ViewHolder(inflate);
         return viewHolder;
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
 
+        PLModel plModel = arrayList.get(position);
+
+        holder.NamePL.setText(plModel.getName());
+        holder.startDate.setText(plModel.getCurrentSeason().getStartDate());
+        holder.endDate.setText(plModel.getCurrentSeason().getEndDate());
+        holder.matchDay.setText(plModel.getCurrentSeason().getCurrentMatchday());
 
 
     }
 
     @Override
-    public int getItemCount() {
-        return 0;
+    public int getItemCount()
+    {
+        return arrayList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        TextView tvNamePL;
-        TextView tvstartDate;
-        TextView tvendDate;
-        TextView tvmatchDay;
+        TextView NamePL;
+        TextView startDate;
+        TextView endDate;
+        TextView matchDay;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            tvNamePL = itemView.findViewById(R.id.tvNamePL);
-            tvstartDate = itemView.findViewById(R.id.tvstartDate);
-            tvendDate = itemView.findViewById(R.id.tvendDate);
-            tvmatchDay = itemView.findViewById(R.id.tvMatchday);
+            NamePL = itemView.findViewById(R.id.tvNamePL);
+            startDate = itemView.findViewById(R.id.tvstartDate);
+            endDate = itemView.findViewById(R.id.tvendDate);
+            matchDay = itemView.findViewById(R.id.tvMatchday);
 
 
         }
